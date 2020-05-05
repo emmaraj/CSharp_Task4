e73651e7d855d99df0b7c281c69999bf301a8695
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-05 20:53:59 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Developed by Mbaya Emmanuel Baldwin (@emmaraj)
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -7,55 +9,70 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Task5.Models;
 
-namespace Task5.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace Task5.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult Add()
-        {
+        public IActionResult Add() {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Add(string firstNumber, string secondNumber)
-        {
+        public IActionResult Add(string firstNumber, string secondNumber) {
             ViewBag.Result = square(firstNumber, secondNumber);
             return View();
         }
 
-        private static String square(string firstNum, string secondNum){
-            int firstNumber = int.Parse(firstNum);
-            int secondNumber = int.Parse(secondNum);
+        private static String square(string firstNum, string secondNum) {
+            int firstNumber;
+            int secondNumber;
             string output = string.Empty;
 
 
-            if(firstNum == "" || secondNum == ""){
+            if (firstNum == "" || secondNum == "") {
                 output = "Kindly input values";
-            }else if(fi)
+            }
+            else if (int.TryParse(firstNum, out firstNumber) && (int.TryParse(secondNum, out secondNumber))) {
+                if (firstNumber < 0 || secondNumber < 0) {
+                    output = "Enter only positive numbers i.e. numbers above zero";
+                }
+                else {
+
+                    double firstNumberSquareRoot = Math.Sqrt(firstNumber);
+                    double secondNumberSquareRoot = Math.Sqrt(secondNumber);
+
+                    if (firstNumberSquareRoot > secondNumberSquareRoot) {
+                        output = "The number " + firstNumber + " with Square root " + firstNumberSquareRoot + " has a higher square root than the number " + secondNumber + " with square root " + secondNumberSquareRoot;
+                    }
+                    else if (firstNumberSquareRoot < secondNumberSquareRoot) {
+                        output = "The number " + secondNumber + " with Square root " + secondNumberSquareRoot + " has a higher square root than the number " + firstNumber + " with square root " + firstNumberSquareRoot;
+                    }
+                    else if (firstNumberSquareRoot == secondNumberSquareRoot) {
+                        output = "You inputted similiar numbers, kindly enter different numbers";
+                    }
+                }
+            }
+            else {
+                output = "Only numbers are accepted please";
+            }
 
             return output;
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
